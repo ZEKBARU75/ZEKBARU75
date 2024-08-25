8b9c7bc76ab4baa5c3a56798fab14cc653df5354
ZEKBARU75 <ysbvsb13@gmail.com>
2024-08-25 21:14:30 +0700

    Add files via upload

---
+++ scgabut.sh
@@ -0,0 +1,133 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+
+toilet -f big -F gay WELCOME!!!
+echo $green"selamat datang"
+echo
+sleep 2
+echo $yellow"silahkan pilih menunya ya!"
+echo 
+echo $yellow"0.) keluar"
+echo
+echo $green"1.) tols spam wa terbaru verzi orang"
+echo
+echo $red"2.) nomor yang bisa di jadikan korban tidak di sarankan"
+echo
+echo $white
+read -p "pilih mana : " bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+toilet -f big -F gay ini scnya
+sleep 2
+echo $green 
+echo subrek dulu chanel w
+sleep 3
+xdg-open https://youtube.com/@zek_baru_telah_terlala_papi?si=WOXo-r1shE9SUby7
+sleep 6
+clear
+toilet -f big -F gay SCRIPTNYA
+sleep 2
+xdg-open https://mkomsel.com/download/file/01J64PCM08NE5XMVGSY7XYZ7WF/0YuNb7AJRosqdYxg
+echo $yellow
+echo link yang anda masuki itu link untuk unduh cara spam wa 
+echo jika link yang tadi anda cuekin kamu bisa memasukinya lagi dalam beberapa detik atau di akhir
+echo hmm ini bukan punya w saya hanya membagikan punya 
+sleep 3
+echo orang saja jadi mohon maaf ya jika w tidak memberikan tutorialnya
+echo $cyan
+sleep 3
+echo token scriptnya:
+echo Mx4aUgbSMRycd5UoQUQzwhAzjpQPlTHhnXAasFZBxNCKNjMrH
+sleep 2
+echo sc yang sedang kalian masuki saat ini akan keluar dalam 20 detik lagi
+echo https://mkomsel.com/download/file/01J64PCM08NE5XMVGSY7XYZ7WF/0YuNb7AJRosqdYxg
+echo itu link download yang tadi
+sleep 42
+clear
+echo waktu habis anda keluar secara paksa oleh sistem
+echo maaf
+sleep 6
+clear
+sleep 4
+toilet -f big -F gay ASU!!!
+sleep 2
+clear
+echo MAAF HEHE 
+sleep 3
+clear
+echo ini script gabut bang maaf
+sleep 3
+clear
+echo pesan CHANEL YANG ANDA KUNJUNGI SECARA PAKSA SUDAH PENSI!!!
+toilet -f big -F gay GOOD LUCK!!!
+echo semoga yang kalian buat dengan spam wa ke orang
+echo tidak sampai di gebug warga yang jadi korban jika ketahuan
+exit
+fi
+
+
+if [ $bro = 0 ] || [ $bro = 0 ]
+then
+toilet -f big -F gay BAY!
+sleep 5
+clear
+sleep 3
+exit
+fi
+
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+echo nomor1:+62 822-6634-7760
+echo nomor2:+62 851-9858-4603
+echo nomor3:+62 823-5108-1275
+echo nomor paling boleh di spam nomor 2 dia sering minta di spam 
+echo itu dia nomor yang bisa di jadikan target
+sleep 3
+echo pesan sc yang kalian masuki ini akan di tutup dalam 15 detik 
+sleep 32
+clear
+echo waktu sudah habis
+sleep 4
+clear
+echo pesan  maaf jika hanya 3 nomor takutnya pada emosi ke kalian
+echo jika ketahuan
+sleep 3
+clear
+echo by semogaa bermanfaat nomor targetnya
+sleep 3
+clear
+sleep 3
+toilet -f big -F gay GOOD BY
+sleep 5
+clear
+exit
+fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
